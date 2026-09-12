c101913f0bfa3841dcc22a059aaf30f7a4dacd27
Leandro Simões <lesimoes0@gmail.com>
2026-09-12 17:40:46 -0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.lesimoes.com',
+    site: 'https://lesimoes.com',
     integrations: [mdx(), sitemap(), react()],
     vite: {
         plugins: [tailwindcss()],
